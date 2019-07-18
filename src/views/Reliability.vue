c9867a40c4c692f1ca8cd04099af8d10887dc97e
redtxai <redtxai@gmail.com>
2019-07-18 15:48:12 -0300

    feat(reliability) Finish reliability page

--- src/views/Reliability.vue
+++ src/views/Reliability.vue
@@ -1,13 +1,42 @@
 <template>
   <article class="reliability">
     <h3>Termo de Confiabilidade</h3>
-    <embed :src="require('@/assets/files/reliability-terms.pdf')">
+    <section class="pdf">
+      <pdf v-for="i in numPages" :key="i" :src="src" :page="i"></pdf>
+    </section>
+    <section class="signature"></section>
+    <button @click="next">NEXT</button>
   </article>
 </template>
 
 <script>
+import pdf from "vue-pdf";
+
+const loadingTask = pdf.createLoadingTask(
+  require("@/assets/files/reliability-terms.pdf")
+);
+
 export default {
-  name: "Reliability"
+  name: "Reliability",
+  components: {
+    pdf
+  },
+  data() {
+    return {
+      src: loadingTask,
+      numPages: undefined
+    };
+  },
+  mounted() {
+    this.src.then(pdf => {
+      this.numPages = pdf.numPages;
+    });
+  },
+  methods: {
+    next() {
+      this.$router.push("/thank-you");
+    }
+  }
 };
 </script>
 
@@ -21,6 +50,7 @@ export default {
   display: flex;
   flex-direction: column;
   justify-content: space-around;
+  align-items: center;
 
   h3 {
     margin: 0;
@@ -29,9 +59,16 @@ export default {
     font-size: 25px;
   }
 
-  embed {
+  .pdf {
     width: 100%;
     height: 35%;
+    overflow: auto;
+  }
+
+  .signature {
+    width: 100%;
+    height: 20%;
+    border: 1px solid $border-signature;
   }
 }
 </style>
